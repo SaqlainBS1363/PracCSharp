6b7dfc4406f7ba0a2dd856216bc36a0d9433f75d
Md. Anwarul Habib <anwarul.habib@brainstation-23.com>
2023-03-27 00:48:22 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int num1 = 10;
+            long num2 = 1000000000;
+            float num3 = 5.140f;
+            double num4 = 63.25697458;
+            char num5 = 'a';
+            string num6 = "CSE";
+            bool num7 = true;
+
+            Console.WriteLine("Int: " + num1);
+            Console.WriteLine("Long: " + num2);
+            Console.WriteLine("Float: " + num3);
+            Console.WriteLine("Double: " + num4);
+            Console.WriteLine("Char: " + num5);
+            Console.WriteLine("String: " + num6);
+            Console.WriteLine("Bool: " + num7);
+
+
+        }
+    }
+}
