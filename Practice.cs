38ee45c3c0f1efb6a082b655718abf4029706334
SaqlainBS1363 <anwarul.habib@brainstation-23.com>
2023-04-03 12:57:22 +0600

    C# Struct task

---
+++ Practice.cs
@@ -0,0 +1,18 @@
+﻿namespace Task1
+{
+    struct Practice
+    {
+        public string Name, ID;
+        public void Initialize(string Name, String ID)
+        {
+            this.Name = Name;
+            this.ID = ID;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Welcome " + Name);
+            Console.WriteLine("Your ID: " + ID);
+        }
+    }
+}
