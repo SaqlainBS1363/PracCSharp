f30f759b98702252d35df9d68929043d3a5a949e
SaqlainBS1363 <anwarul.habib@brainstation-23.com>
2023-04-03 12:56:25 +0600

    C# Class task

---
+++ CSharpBasics.cs
@@ -0,0 +1,23 @@
+﻿namespace Task1
+{
+    internal partial class Program
+    {
+        public class CSharpBasics
+        {
+            public string traineeID;
+            public string traineeName;
+            
+            public CSharpBasics(string traineeID, string traineeName)
+            {
+                this.traineeID = traineeID;
+                this.traineeName = traineeName;
+            }
+
+            public void Introduce()
+            {
+                Console.WriteLine("Hello " + traineeName + ", welcome to BS23");
+                Console.WriteLine("Your ID is: " + traineeID);
+            }
+        }
+    }
+}
